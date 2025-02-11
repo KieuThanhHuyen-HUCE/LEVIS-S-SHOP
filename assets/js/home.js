c23fe0309c2f8ed263f7aae0ff4078b112cec99c
KieuThanhHuyen-HUCE <kieuthanhhuyen673@gmail.com>
2025-02-11 13:51:34 +0700

    update file

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
     let container = $(".list-colection"); 
     let itemWidth = $(".list-colection .item").outerWidth(true); 
-
-    $("#next-product").click(function () {
+    $(".next-product").click(function () {
         container.animate({ scrollLeft: "+=" + itemWidth }, 400);
-    });
 
-    $("#prev-product").click(function () {
-        container.animate({ scrollLeft: "-=" + itemWidth }, 400);
     });
+    $(".prev-product").addClass("disable-icon")
+    $(".prev-product").click(function () {
+        let $this = $(this)
+        container.animate({ scrollLeft: "-=" + itemWidth }, 400, function(){
+            if(container.scrollLeft() <= 0){
+                $(".prev-product").removeClass("disable-icon")
+            }
+        });
+    });
+    $(".disable-icon").hover(function(){
+        $(".disable-notify").toggle()
+    })
 });
